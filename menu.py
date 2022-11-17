d3aa1911c2824c7735343e9ddc12a2746645742a
Samandar000 <samandarnfh.2020@gmail.com>
2022-11-18 03:09:51 +0500

    catalog

--- menu.py
+++ menu.py
@@ -2,7 +2,8 @@ from telegram.ext import(
     Updater,
     CommandHandler,
     MessageHandler,
-    Filters
+    Filters,
+    CallbackQueryHandler
 )
 from telegram import KeyboardButton,ReplyKeyboardMarkup,InlineKeyboardButton,InlineKeyboardMarkup
 def start(update,context):
@@ -33,9 +34,39 @@ def echo(update,context):
     
     bot.sendMessage(chat_id,text,reply_markup = keyboard)
 
+def inlinekeyboard(update, context):
+    chat_id = update.message.chat.id
+    text = update.message.text
+    bot = context.bot
+
+    pizza = InlineKeyboardButton(
+        text='pizza🍕',
+        callback_data='pizza'
+        )
+    keyboard = InlineKeyboardButton([[pizza]])
+    text = '🏬Catalog'
+    
+    bot.sendMessage(chat_id,text=text,reply_murkup = keyboard )
+
+def catalog(update,context):
+    print(1)
+    query = update.callback_query
+    catalog = InlineKeyboardButton(
+        text='🍕 pizza',
+        switch_inline_query_current_chat='🍕 pizza'
+    )
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [catalog]
+        ]
+    )
+    update.message.reply_text(text='🏬 Catalog',reply_markup=reply_markup)
 updater = Updater('5643654386:AAGaxNP-8Kkwzi8Ko047p0BZBd3t6a0eIu4')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+updater.dispatcher.add_handler(MessageHandler(Filters.text('🏬 Catalog'),catalog))
+# updater.dispatcher.add_handler(MessageHandler(Filters.text('👤 Userinfo'),userinfo))
+# updater.dispatcher.add_handler(MessageHandler(Filters.text('🎛 Administration'),administration))
 updater.start_polling()
 updater.idle()
